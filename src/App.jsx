a2ef895a6126e83c05e1c00bfb0443ed97400fb6
ShahriarMurtoza <shahriarmurtoza.dev@gmail.com>
2026-04-16 01:03:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './assets/components/navbar.jsx';
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+
+    </div>
+  );
+};
+
+export default App;
